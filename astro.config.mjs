c61a129f84d6e40924418e294127518a4de6d0ca
Vinicius Gomes <vinicius.svgomes@gmail.com>
2026-08-31 21:37:17 -0300

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vinisilvag.github.io",
-  integrations: [react(), tailwind()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
