ae0e433bbef2f57bee2a212ec65681edff85b4ff
Vinicius Gomes <vinicius.svgomes@gmail.com>
2026-09-06 12:57:17 -0300

    Better SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vinisilvag.github.io",
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
